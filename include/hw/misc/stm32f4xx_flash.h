6a7a3523f219352f8a4c4b4b9375ff71599d412d
zhouyuming <1006129869@qq.com>
2025-10-27 07:53:39 +0800

    Create stm32f4xx_flash.h

---
+++ include/hw/misc/stm32f4xx_flash.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 liang yan <yanl1229@163.com>
+ *
+ * STM32F1xx Flash interface
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributions after 2012-01-13 are licensed under the terms of the
+ * GNU GPL, version 2 or (at your option) any later version.
+ */
+#ifndef STM32F4XX_FLASH_H
+#define STM32F4XX_FLASH_H
+
+#include "hw/sysbus.h"
+
+#define STM_FLASH_ACR      0x00
+#define STM_FLASH_KEYR     0x04
+#define STM_FLASH_OPTKEYR  0x08
+#define STM_FLASH_SR       0x0c
+#define STM_FLASH_CR       0x10
+#define STM_FLASH_AR       0x14
+#define STM_FLASH_OBR      0x18
+#define STM_FLASH_WRPR     0x20
+
+#define PWR_CR_DBP      (1 << 8)
+
+#define TYPE_STM32F4XX_FLASH "stm32f4xx-flash"
+#define STM32F4XX_FLASH(obj) \
+    OBJECT_CHECK(STM32F4XXFlashState, (obj), TYPE_STM32F4XX_FLASH)
+
+typedef struct {
+    /* <private> */
+    SysBusDevice parent_obj;
+
+    /* <public> */
+    MemoryRegion mmio;
+
+    uint32_t flash_acr;
+    uint32_t flash_keyr;
+    uint32_t flash_optkeyr;
+    uint32_t flash_sr;
+    uint32_t flash_cr;
+    uint32_t flash_ar;
+    uint32_t flash_obr;
+    uint32_t flash_wrpr;
+
+} STM32F4XXFlashState;
+
+#endif
